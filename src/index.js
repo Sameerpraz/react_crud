13727bf05e3eb65c03826bb47953b4626fa3e536
Sameerpraz <samirpar788@gmail.com>
2021-07-27 13:34:24 +0545

    redux crud

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from "redux";
+import { Provider } from 'react-redux';
+import { transactionReducer } from './reducers/transactionReducer';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if(localStorage.getItem('transactions') == null)
+    localStorage.setItem('transactions', JSON.stringify([]))
+let initialState ={
+  currentIndex: -1,
+  list : JSON.parse(localStorage.getItem('transactions'))
+}
+
+var store = createStore(transactionReducer, initialState)
+ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
